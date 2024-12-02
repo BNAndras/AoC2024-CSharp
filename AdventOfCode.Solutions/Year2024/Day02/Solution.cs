490ec0ce65d8daf4d75ffc796d1d54550bbe2538
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-12-01 21:56:30 -0800

    D02

--- AdventOfCode.Solutions/Year2024/Day02/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day02/Solution.cs
@@ -2,15 +2,45 @@ namespace AdventOfCode.Solutions.Year2024.Day02;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(02, 2024, "") { }
 
-    protected override string SolvePartOne()
+    private List<List<int>> Reports { get; }
+    
+    public Solution() : base(02, 2024, "Red-Nosed Reindeer")
     {
-        return "";
+        Reports = Input
+            .SplitByNewline()
+            .Select(line =>
+                line.Split()
+                    .Select(int.Parse)
+                    .ToList()
+            ).ToList();
     }
 
-    protected override string SolvePartTwo()
+    private bool IsSorted(List<int> levels) => IsDescending(levels) || IsAscending(levels);
+    
+    private bool IsDescending(List<int> levels) =>
+        Enumerable
+            .Range(0, levels.Count - 1)
+            .All(i => levels[i] >= levels[i + 1] && levels[i] - levels[i + 1] is >= 1 and <= 3);
+    
+    private bool IsAscending(List<int> levels) =>
+        Enumerable
+            .Range(0, levels.Count - 1)
+            .All(i => levels[i] <= levels[i + 1] && levels[i + 1] - levels[i] is >= 1 and <= 3);
+
+    private bool IHaveAHeadache(List<int> levels)
     {
-        return "";
+        if (IsSorted(levels)) return true;
+
+       return Enumerable.Range(0, levels.Count)
+            .Select(i => levels.Where((_, index) => i != index)).Any(x => IsSorted(x.ToList()));
     }
+        
+    protected override string SolvePartOne() =>
+        Reports.Count(IsSorted).ToString();
+    
+    protected override string SolvePartTwo() =>
+        Reports
+            .Count(IHaveAHeadache)
+            .ToString();
 }
