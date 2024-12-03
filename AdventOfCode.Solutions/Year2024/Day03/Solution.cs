d14fb3dccfa78d331530ec0bbccc35770b7805b7
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-12-02 22:29:04 -0800

    D03

--- AdventOfCode.Solutions/Year2024/Day03/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day03/Solution.cs
@@ -1,16 +1,29 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode.Solutions.Year2024.Day03;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(03, 2024, "") { }
+    private Regex FindMul { get; } = new(@"mul\((\d+),(\d+)\)");
 
-    protected override string SolvePartOne()
-    {
-        return "";
-    }
+    private Regex FindNewlines { get; } = new(@"\t|\n|\r");
+    private Regex FindUndoSections { get; } = new(@"(?:don't\(\).*?(?:(?:do\(\))|$))");
+    
+    public Solution() : base(03, 2024, "Mull It Over") { }
+
+    protected override string SolvePartOne() =>
+        FindMul
+            .Matches(Input)
+            .Sum(m => int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value))
+            .ToString();
 
     protected override string SolvePartTwo()
     {
-        return "";
+        string cleaned = FindNewlines.Replace(Input, "");
+        cleaned = FindUndoSections.Replace(cleaned, "");
+        return FindMul
+            .Matches(cleaned)
+            .Sum(m => int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value))
+            .ToString();
     }
 }
