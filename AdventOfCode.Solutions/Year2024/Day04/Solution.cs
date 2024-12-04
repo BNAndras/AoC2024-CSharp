2026e3a66f618cf708b3e4e01d7a516feec9ea15
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-12-03 21:58:08 -0800

    D04

--- AdventOfCode.Solutions/Year2024/Day04/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day04/Solution.cs
@@ -2,15 +2,77 @@ namespace AdventOfCode.Solutions.Year2024.Day04;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(04, 2024, "") { }
+    
+    private List<List<char>> Grid { get; }
+
+    private (string name, int x, int y)[] P1Directions =>
+    [
+        ("N", 0, 1),
+        ("S", 0, -1),
+        ("E", 1, 0),
+        ("W", -1, 0),
+        ("NE", 1, 1),
+        ("NW", -1, 1),
+        ("SE", 1, -1),
+        ("SW", -1, -1),
+    ];
+    
+    public Solution() : base(04, 2024, "Ceres Search")
+    {
+        Grid = Input.SplitByNewline().Select(x => x.ToCharArray().ToList()).ToList();
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        int count = 0;
+        int maxY = Grid.Count - 1;
+        int maxX = Grid[0].Count - 1;
+        for (var y = 0; y <= maxY; y++)
+        {
+            for (var x = 0; x <= maxX; x++)
+            {
+                if (Grid[y][x] is not 'X') continue;
+                foreach (var (_, dx, dy) in P1Directions)
+                {
+                    if (0 > (y + dy * 3) || (y + dy * 3) > maxY) continue;
+                    if (0 > (x + dx * 3) || (x + dx * 3) > maxX) continue;
+
+                    if (Grid[y + dy][x + dx] is 'M' && Grid[y + dy * 2][x + dx * 2] is 'A' &&
+                        Grid[y + dy * 3][x + dx * 3] is 'S')
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count.ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        int count = 0;
+        int maxY = Grid.Count - 1;
+        int maxX = Grid[0].Count - 1;
+        for (var y = 1; y < maxY; y++)
+        {
+            for (var x = 1; x < maxX; x++)
+            {
+                if (Grid[y][x] is not 'A') continue;
+                
+                char upperLeft = Grid[y - 1][x - 1];
+                char upperRight = Grid[y - 1][x + 1];
+                char lowerLeft = Grid[y + 1][x - 1];
+                char lowerRight = Grid[y + 1][x + 1];
+
+                if (((upperLeft is 'M' && lowerRight is 'S') || (upperLeft is 'S' && lowerRight is 'M')) &&
+                    ((upperRight is 'M' && lowerLeft is 'S') || (upperRight is 'S' && lowerLeft is 'M')))
+                {
+                    count++;
+                }
+            }
+        }
+        
+        return count.ToString();
     }
 }
